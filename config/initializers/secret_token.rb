c978a06f9a9a1ce06dffd18b35230d119dcdaaf7
Austin Forest <cforest@pattonboggs.com>
2012-07-16 08:06:59 -0600

    A gem which I am not sure what it will be yet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lottery::Application.config.secret_token = 'd4d0d45d2f7f5604a9c101d552e815d5863996ad60afbac86a5f10d567b384aa538bc59cac59b1805bb0862bbcd6467973dd1153c5b8f93da3497dd945d32298'
